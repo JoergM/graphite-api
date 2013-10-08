c03a69a1a52c36afaf99b8b7fcdef60cff842175
Joerg Mueller <joergm42@gmail.com>
2013-10-08 07:55:18 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to graphite-api
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/graphite_api/core.clj
@@ -0,0 +1,11 @@
+(ns graphite-api.core)
+
+(defmacro urlencode [param] `(java.net.URLEncoder/encode (str ~param)))
+
+(defn build-query-string [parameter-map]
+  (apply str "?"
+         (interpose "&"
+                    (map #(str (urlencode (name (key %)))
+                               "="
+                               (urlencode (val %)))
+                         parameter-map))))
