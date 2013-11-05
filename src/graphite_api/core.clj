bb69d4dbe744477b27a67cc2c5ed5b3db3f1a5c0
Joerg Mueller <joergm42@gmail.com>
2013-11-05 08:03:27 +0100

    Data is now transformed to a more Clojure-friendly structure.

--- src/graphite_api/core.clj
+++ src/graphite_api/core.clj
@@ -10,11 +10,11 @@
 (defn- build-query-string [parameter-map]
   (apply str "?"
          (interpose
-           "&"
-           (map #(str (urlencode (name (key %)))
-                      "="
-                      (urlencode (val %)))
-                parameter-map))))
+          "&"
+          (map #(str (urlencode (name (key %)))
+                     "="
+                     (urlencode (val %)))
+               parameter-map))))
 
 (defn- build-full-url [server port parameter-map]
   (str "http://" server ":" port "/render" (build-query-string parameter-map)))
@@ -22,24 +22,36 @@
 (defn- get-server-response [url]
   (:body (client/get url)))
 
+(defn- filter-nil-tuples [array]
+  (filter #(first %) array))
+
+(defn- convert-dates [array]
+  (map #(vector (first %) (Date. (* 1000 (second %)))) array))
+
 (defn load-data
+  "Returns the data stored in graphite as a vector of tuples.
+  Removes elements where values are nil."
   [data-key & {:keys [host port]
                :or {host "localhost" port "80"}}]
   (-> (build-full-url host port {:target data-key :format "json"})
       get-server-response
-      json/read-str))
+      json/read-str
+      (get 0)
+      (get "datapoints")
+      filter-nil-tuples
+      convert-dates))
 
-;; next try with some data and convert data-information to real date and to map-key
+;; next add parameters for data-range
 
 ;; sending logic
 (defn- build-message [^String key
-                     ^Number value
-                     ^Long timestamp]
+                      ^Number value
+                      ^Long timestamp]
   (str key " " value " " (long (/ timestamp 1000)) "\n"))
 
 (defn send-data-point [key value & {:keys [host port timestamp]
-                              :or {host "localhost"
-                                   port 2003
-                                   timestamp (.getTime (Date.))}}]
-    (spit (Socket. host port)
-          (build-message key value timestamp)))
+                                    :or {host "localhost"
+                                         port 2003
+                                         timestamp (.getTime (Date.))}}]
+  (spit (Socket. host port)
+        (build-message key value timestamp)))
